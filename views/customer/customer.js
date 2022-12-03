0fbe337b1581c1214f35b09ba1ec323e5ade8dce
YUN.CHHILIN <YUN.CHHILIN@pnc.lan>
2022-12-03 18:50:04 +0700

    [customer.js] search bar

--- views/customer/customer.js
+++ views/customer/customer.js
@@ -1,105 +1,88 @@
-function createCard(name,description,price, currency,image){
-    
-  
+function createCard(name, description, price, currency, image) {
 
     const card = document.createElement("div");
     card.className = "card";
 
     const icon = document.createElement("div");
     icon.className = "icon";
+    console.log(card)
 
     const img = document.createElement("img");
     img.src = image;
- 
-    // const productName = document.createElement("div")
 
     const text1 = document.createElement("div");
     text1.className = "text";
-    text1.textContent = price+currency
-    
+    text1.textContent = price + currency
+
+
     const text2 = document.createElement("div");
     text2.className = "text";
-    text2.textContent = name +  ": " + description;
-    // text2.textContent = "text"
-    // console.log(text1)
+    const span = document.createElement("span");
+    span.textContent = name;
+    text2.appendChild(span)
 
+    const text = document.createElement("div");
+    text.className = "text";
+    text.textContent = description;
 
+    const icon_fa_start = document.createElement("div");
+    icon_fa_start.className = "icon";
+    const firstI = document.createElement("i");
+    firstI.className = "fa fa-star";
+    const secendI = document.createElement("i");
+    secendI.className = "fa fa-star";
+    const thirdI = document.createElement("i");
+    thirdI.className = "fa fa-star";
+    const fourthI = document.createElement("i");
+    fourthI.className = "fa fa-star";
 
-    // const icon_fa_start = document.createElement("div");
-    // icon_fa_start.className = "fa fa-star";
+    icon_fa_start.appendChild(firstI)
+    icon_fa_start.appendChild(secendI)
+    icon_fa_start.appendChild(thirdI)
+    icon_fa_start.appendChild(fourthI)
 
     const button = document.createElement("button");
     button.textContent = "Buy Now"
-
 
     card.appendChild(img)
     card.appendChild(button)
     card.appendChild(text1)
     card.appendChild(text2)
+    card.appendChild(text)
+    card.appendChild(icon_fa_start)
 
-    // icon.appendChild(icon_fa_start)
     icon.appendChild(button)
- 
-
-  
-
     card.appendChild(text1)
     card.appendChild(icon)
-  
-    
 
     return card;
 }
-function displayProduct(){
+// -------------------------search bar--------------
+function btnSearch() {
+    let text = search.value.toLowerCase();
+    let tasks = document.querySelectorAll(".card");
+    for (let task of tasks) {
+        let taskTitle = task.children[1].textContent.toLowerCase();
+        if (taskTitle.indexOf(text) === -1) {
+            task.style.display = "none";
+        } else {
+            task.style.display = "block";
+        }
+    }
+}
+let search = document.querySelector("#search");
+search.addEventListener("keyup", btnSearch)
+
+function displayProduct() {
     let products = JSON.parse(localStorage.getItem("product-name")) ?? [];
     const main = document.createElement("div");
     main.className = "main";
     for (let product of products) {
-        let card = createCard(product.name, product.description ,product.currency,product.price , product.img);
+        let card = createCard(product.name, product.description, product.currency, product.price, product.img);
         main.appendChild(card)
     }
     container.appendChild(main)
-    
 }
 
 const container = document.querySelector(".container");
 document.addEventListener('DOMContentLoaded', () => { displayProduct(); })
-
-
-
-
-
-
-
-
-
-
-
-
-// let product_name = document.querySelector("#product_name")
-// let product_descrition = document.querySelector("#product_descrition");
-// let product_price = document.querySelector("#product_price");
-// let product_currency = document.querySelector("#product_currency");
-// let product_image = document.querySelector("#product_image")
-// let productBtn = document.querySelector("#btn_save");
-
-// let my_product = document.querySelector("#my_product");
-
-// productList = []
-// productBtn.addEventListener("click",(e)=>{
-//     e.preventDefault()
-//     let productObject = {name:product_name.value, description:product_descrition.value, price:product_price.value, currency:product_currency.value, image:product_image.value}
-//     productList.push(productObject)
-//     console.log(productList)
-
-//     localStorage.setItem("product_name", JSON.stringify(productList))
-   
-// })
-
-// let product = JSON.parse(localStorage.getItem("product_name"));
-// let outPut = ""
-// for (let val of product){
-//     outPut += val.name + " " + val.description + " " + val.price + " " + val.currency + " " + val.image
-// }
-// my_product.textContent = outPut
-// console.log(my_product)
--- views/seller/js/seller.js
+++ views/seller/js/seller.js
@@ -5,30 +5,27 @@ function addProductToLocalStorage(key, value) {
 function getProductFromLocalStorage(key) {
     return JSON.parse(localStorage.getItem(key)) ?? [];
 }
+function createNewRecord(index, name, description, price, currency, img) {
 
-function createNewRecord(name, description, price, currency, img) {
     const tr = document.createElement('tr');
+    tr.dataset.index = index;
     const tdName = document.createElement('td');
     const tdDescription = document.createElement('td');
     const tdPrice = document.createElement('td');
     const tdcurrency = document.createElement('td');
-    const tdImage = document.createElement('td');
     const tdDelete = document.createElement('td');
     const tdEdite = document.createElement('td');
-
     tdName.textContent = name;
     tdDescription.textContent = description;
     tdPrice.textContent = price;
     tdcurrency.textContent = currency;
-    // tdImage.textContent = img;
-
 
     let deletProduct = document.createElement("img")
     deletProduct.className = "my_image"
     deletProduct.src = "../seller/images/delete.png"
 
-
     //            -----------delete-----------
+
     deletProduct.addEventListener("click", (e) => {
         let index = e.target.parentElement.parentElement.dataset.index;
         productList.splice(index, 1);
@@ -43,24 +40,17 @@ function createNewRecord(name, description, price, currency, img) {
     editeProduct.className = "edite"
     editeProduct.src = "../seller/images/edite.png"
 
-    let contain = document.querySelector(".contain");
-    let form_edit = document.querySelector(".form_edit");
     editeProduct.addEventListener("click", (e) => {
-        form_edit.style.display = "block"         
-        contain.style.display = "none"
+        let index = e.target.parentElement.parentElement.dataset.index
+        document.querySelector("#product_name").value = name
+        document.querySelector("#product_descrition").value = description;
+        document.querySelector("#product_price").value = price;
+        document.querySelector("#product_currency").value = currency;
+        document.querySelector("#product_image").value = img;
+
+        productList.splice(index, 1);
+        displayProduct()
     })
-    contain.style.display = "block"
-    form_edit.style.display = "none"
-
-    let dialog_close = document.querySelector("#dialog_close");
-    dialog_close.addEventListener("click",(e)=>{
-        form_edit.style.display = "none"
-        contain.style.display = "block"
-
-        
-    })
-
-
 
     tdDelete.appendChild(deletProduct)
     tdEdite.appendChild(editeProduct)
@@ -68,16 +58,10 @@ function createNewRecord(name, description, price, currency, img) {
     tr.appendChild(tdDescription);
     tr.appendChild(tdPrice);
     tr.appendChild(tdcurrency);
-    // tr.appendChild(tdImage);
     tr.appendChild(tdDelete);
     tr.appendChild(tdEdite);
-    // console.log(tr)
-
     return tr;
-
-
 }
-
 
 function createTableHeader() {
     const headerRow = document.createElement('tr');
@@ -85,7 +69,6 @@ function createTableHeader() {
     const thDescription = document.createElement('th');
     const thPrice = document.createElement('th');
     const thcurrency = document.createElement('th');
-    // const thImage = document.createElement('th');
     const thDelete = document.createElement('th');
     const thEdite = document.createElement('th');
 
@@ -93,7 +76,6 @@ function createTableHeader() {
     thDescription.textContent = "description";
     thPrice.textContent = "price";
     thcurrency.textContent = "currency";
-    // thImage.textContent = "img";
     thDelete.textContent = "delete";
     thEdite.textContent = "edite";
 
@@ -101,47 +83,45 @@ function createTableHeader() {
     headerRow.appendChild(thDescription);
     headerRow.appendChild(thPrice);
     headerRow.appendChild(thcurrency);
-    // headerRow.appendChild(thImage);
     headerRow.appendChild(thDelete);
     headerRow.appendChild(thEdite);
 
-
     return headerRow;
-
 }
-function displayProduct() {
 
+function displayProduct() {
     if (tableData.firstElementChild !== null) {
         document.querySelector('table').remove();
     }
     const newTable = document.createElement('table');
     newTable.appendChild(createTableHeader());
     let products = getProductFromLocalStorage('product-name');
+    console.log(products)
+    for (let index = 0; index < products.length; index++) {
+        let product = products[index]
 
-    for (let product of products) {
-        let row = createNewRecord(product.name, product.description, product.price, product.currency);
+        let row = createNewRecord(index, product.name, product.description, product.price, product.currency, product.img);
         newTable.appendChild(row)
     }
     tableData.appendChild(newTable);
-
 }
-
 
 const tableData = document.querySelector(".table_data")
 const my_product = document.querySelector("#my_product");
-const btn_save = document.querySelector("#btn_save")
+const button_save = document.querySelector("#save")
 
 const product_name = document.querySelector("#product_name")
 const product_descrition = document.querySelector("#product_descrition");
 const product_currency = document.querySelector("#product_currency");
 const product_image = document.querySelector("#product_image")
+const product_price = document.querySelector("#product_price")
 
 let productList = JSON.parse(localStorage.getItem('product-name')) ?? [];
 console.log(productList)
-btn_save.addEventListener("click", (e) => {
+button_save.addEventListener("click", (e) => {
     e.preventDefault();
     if (product_name.value == "" || product_descrition.value == "" || product_currency.value == "" || product_image.value == "" || product_price.value == "") {
-        window.confirm("You didn't select all")
+        window.confirm("You must select all")
         return;
     }
     let productObject = { name: product_name.value, description: product_descrition.value, price: product_price.value, currency: product_currency.value, img: product_image.value }
@@ -155,35 +135,11 @@ btn_save.addEventListener("click", (e) => {
 
     addProductToLocalStorage('product-name', JSON.stringify(productList))
     displayProduct();
-
-
 })
 document.addEventListener('DOMContentLoaded', () => { displayProduct() })
 
 
-// ---------------------edite form-------------
-let form_name = document.getElementById("form_name");
-let form_description = document.getElementById("form_description");
-let form_price = document.getElementById("form_price");
-let form_currency = document.getElementById("form_currency");
-let form_image = document.getElementById("form_image");
-let btnUpdate = document.getElementById("updat");
 
-btnUpdate.addEventListener("click",(e)=>{
-    e.preventDefault()
-if(form_name.value == "" || form_description.value == "" || form_price.value == "" || form_currency.value == "" || form_image.value == ""){
-    window.confirm();
-    return;
-}
-
-let formEdit = {name:form_name.value, description:form_description.value, price:form_price.value};
-productList.push(formEdit)
-console.log(productList)
-addProductToLocalStorage('product-name', JSON.stringify(productList))
-    displayProduct();
-
-})
-document.addEventListener('DOMContentLoaded', () => { displayProduct() })
 
 
 
